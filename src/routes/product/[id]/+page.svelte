85d922fa52cea04d2fa4921995ed6ee9394aa0f3
gafar-code <gafartok@gmail.com>
2024-08-22 09:16:36 +0700

    first commit

---
+++ src/routes/product/[id]/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { page } from '$app/stores';
+
+    $:console.log($page.params.id)
+</script>
+
+<h1>Product page</h1>
+<p>product id: {$page.params.id}</p>
+
+<a href="/product/124">Another product</a>
