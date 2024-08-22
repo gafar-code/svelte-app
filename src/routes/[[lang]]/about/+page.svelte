6bdce5b9b068ae4067533147f78187b47c2d57d7
gafar-code <gafartok@gmail.com>
2024-08-22 09:54:05 +0700

    Add optional parameter

---
+++ src/routes/[[lang]]/about/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { goto } from '$app/navigation';
+    import { page } from '$app/stores';
+</script>
+
+<h1>About page</h1>
+<p>params: {JSON.stringify($page.params)}</p>
+<button on:click={() => goto('/')}>Back to home</button>
+
